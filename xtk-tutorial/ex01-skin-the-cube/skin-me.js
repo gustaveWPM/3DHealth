1c28ca1cfad5e8841a4b2d5ff4d294e4e49a7994
gustaveWPM <jgeorge@perso.be>
2023-07-16 14:09:37 +0200

    add: Skin the cube and rotate it!

---
+++ xtk-tutorial/ex01-skin-the-cube/skin-me.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", () => {
+  function getCubeInstance() {
+    const cube = new X.cube();
+
+    cube.lengthX = cube.lengthY = cube.lengthZ = 20;
+    cube.center = [0, 0, 0];
+    // cube.color = [1, 1, 1];
+    cube.texture.file = './graphics/texture.png';
+    return cube;
+  }
+
+  const renderer = new X.renderer3D();
+  const cube = getCubeInstance();
+  const onRenderFn = () => {
+    cube.transform.rotateX(1);
+    cube.transform.rotateY(-1);
+  }
+
+  renderer.init();
+  renderer.add(cube);
+  renderer.render();
+  renderer.onRender = onRenderFn;
+});
